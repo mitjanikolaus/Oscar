d03655665b7a1115c4ba0502227a93be3a376ce6
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2022-03-23 10:47:58 +0100

    Update imports for VL eval

--- oscar/utils/cbs.py
+++ oscar/utils/cbs.py
@@ -10,7 +10,7 @@ import os.path as op
 import torch
 from typing import Callable, Dict, List, Optional, Tuple
 
-from oscar.modeling.modeling_utils import BeamHypotheses
+from Oscar.oscar.modeling.modeling_utils import BeamHypotheses
 
 StepFunctionType = Callable[
     [torch.Tensor, List[torch.Tensor]], Tuple[torch.Tensor, List[torch.Tensor]]
